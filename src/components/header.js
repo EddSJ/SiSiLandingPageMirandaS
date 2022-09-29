0e0eddd2cd8072387cbf813eb0ca8a7168866d74
Eduardo Sanchez <eddiekidd98@gmail.com>
2022-09-29 14:53:57 -0500

    responsiveness added

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 //logo
 import mirandaLogo from "../assets/miranda-logo.png"
 
 
 const Header = () => {
+
+
+  
+
   return (
     <div className='header-wrapper'>
         <div className='logo-marca-wrapper'>
           <img src={mirandaLogo} alt="logo" />
         </div>
-        <div className='nav-bar'>
-            <a href='#inicio'>Inicio</a> 
-            <a href='#staff'>Quienes somos</a>
-            <a href='#servicios'>Nuestros servicios</a>
+        <div>
+          {/* <FaBars  />  */}
+          <div className='nav-bar'>
+              <a href='#inicio'>Inicio</a> 
+              <a href='#staff'>Quienes somos</a>
+              <a href='#servicios'>Nuestros servicios</a>
+          </div>
         </div>
     </div>
   );
